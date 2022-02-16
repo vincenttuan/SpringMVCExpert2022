fc8efba45b6666c763029c3258925b3bdb96badb
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-02-16 13:40:45 +0800

    Add files via upload

--- src/main/java/spring/mvc/session15/controller/JobController.java
+++ src/main/java/spring/mvc/session15/controller/JobController.java
@@ -1,11 +1,15 @@
 package spring.mvc.session15.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import spring.mvc.session15.entity.Job;
 import spring.mvc.session15.repository.JobDao;
 
 @Controller
@@ -19,6 +23,12 @@ public class JobController {
 	@ResponseBody
 	public String createTable() {
 		return "create job table：" + jobDao.createTable();
+	}
+	
+	@GetMapping(value = "/query_json", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public List<Job> queryJson() {
+		return jobDao.queryAll();
 	}
 }
 
--- src/main/java/spring/mvc/session15/repository/JobDao.java
+++ src/main/java/spring/mvc/session15/repository/JobDao.java
@@ -1,8 +1,17 @@
 package spring.mvc.session15.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import spring.mvc.session15.entity.Employee;
+import spring.mvc.session15.entity.Job;
 
 @Repository
 public class JobDao {
@@ -18,5 +27,37 @@ public class JobDao {
 					+ ")";
 		jdbcTemplate.execute(sql);
 		return true;
+	}
+	
+	// 使用 BeanPropertyRowMapper
+	public List<Job> queryAll() {
+		String sql = "select j.jid, j.jname, j.eid from job j";
+		List<Job> jobs = jdbcTemplate.query(sql, new BeanPropertyRowMapper(Job.class));
+		return jobs;
+	}
+	
+	// 實作 RowMapper
+	/* public interface RowMapper<T> {
+	 *     T mapRow(ResultSet rs, int rowNum) throws SQLException;
+	 * }
+	 * */
+	public List<Job> queryAll2() {
+		String sql = "select j.jid, j.jname, j.eid from job j";
+		RowMapper<Job> rm = (ResultSet rs, int rowNum) -> {
+			Job job = new Job();
+			job.setJid(rs.getInt("jid"));
+			job.setJname(rs.getString("jname"));
+			job.setEid(rs.getInt("eid"));
+			// 再根據 eid 查詢 employee
+			String sql2 = "select e.eid, e.ename, e.salary, e.createtime from employee e where e.eid=?";
+			Employee employee = jdbcTemplate.queryForObject(
+					sql2, 
+					new Object[] {job.getEid()}, 
+					new BeanPropertyRowMapper<Employee>(Employee.class));
+			job.setEmployee(employee);
+			return job;
+		};
+		List<Job> jobs = jdbcTemplate.query(sql, rm);
+		return jobs;
 	}
 }
